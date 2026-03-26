e1e0541fab65e456641b5caba4e457797b6ad597
erico cordeiro neto <ericocneto@MacBook-Pro-de-erico.local>
2026-03-26 16:52:39 -0300

    fix: fix registration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify"
+import cors from "@fastify/cors"
 import routes from "./routes/index.js"
 const app = fastify()
 const PORT = 3333
 
+app.register(cors, { origin: true })
 app.register(routes)
 
 app.listen({
