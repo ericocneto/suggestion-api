61c03ec78efa4b0d62e14fc2176be6ba52bb47ae
erico cordeiro neto <ericocneto@MacBook-Pro-de-erico.local>
2026-03-26 01:10:43 -0300

    chore: install main dependencies

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import { type FastifyInstance } from "fastify";
+import {email, uuid, z} from "zod"
  export default async function routes(app:FastifyInstance) {
     app.get("/", async(req, reply)=>{ 
         return {mesasge:"hello world!"}
     })
     app.post("/create", async(req, reply)=>{
-        reply.statusCode = 201
+        const data = req.body
+        const Suggestion = z.object({
+            email: z.email(),
+            message: z.string()
+         })
+        Suggestion.parse(data)
         return {message: "created"}
     })
 }
